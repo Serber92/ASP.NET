839406acceef3bc1ddac3c1170b0fefd79f38d2d
Andrii Ivanytskyi <andriiivanytskyi@gmail.com>
2019-12-01 14:35:59 -0800

    added MVC_template project

---
+++ MVC_template/Controllers/testController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC_template.Models;
+
+namespace MVC_template.Controllers
+{
+  public class testController : Controller
+  {
+    public IActionResult First()
+    {
+      return View();
+    }
+
+    public IActionResult Second()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
+}
